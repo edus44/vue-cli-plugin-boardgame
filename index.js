7fc1c411f3999ddd76f93aad38ad1891727c568c
edus44 <eduhhg@gmail.com>
2018-10-27 21:23:57 +0200

    Fix transpile

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-module.exports = api => {
+module.exports = (api,opts) => {
   api.chainWebpack(config => {
     config.resolve.extensions.add('.mjs')
+    opts.transpileDependencies.push('vue-cli-plugin-boardgame')
   })
 }
